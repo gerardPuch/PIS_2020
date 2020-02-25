fd365b0c19e745070d0bd47308776b1ce4abf332
Puki <pukipuch@gmail.com>
2020-02-25 18:48:38 +0100

    sessio 2 web intent

--- PIS_Sessio2/app/src/main/java/edu/ub/pis2018/gpuchcam7/pis_sessio2/MainActivity.kt
+++ PIS_Sessio2/app/src/main/java/edu/ub/pis2018/gpuchcam7/pis_sessio2/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.ub.pis2018.gpuchcam7.pis_sessio2
 
 import android.content.Intent
 import android.hardware.display.DisplayManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -26,9 +27,19 @@ class MainActivity : AppCompatActivity() {
     fun action2(view: View) {
         val editText = findViewById<EditText>(R.id.editText)
         val message = editText.text.toString()
-        val intent = Intent(this, second::class.java).apply {
+        val intent2 = Intent(this, second::class.java).apply {
             putExtra(EXTRA_MESSAGE, message)
         }
-        startActivity(intent)
+        startActivity(intent2)
     }
+
+    fun action3(view: View) {
+        showWebPage(view)
+    }
+
+    fun showWebPage(view: View){
+        val intent3 = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"))
+        startActivity(intent3)
+    }
+
 }
