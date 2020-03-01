de4a30b1dd332165249858959a7dd5d0a7b0f2a6
Puki <pukipuch@gmail.com>
2020-03-01 11:42:42 +0100

    s

---
+++ MHealth/app/src/main/java/edu/ub/pis2020/gpuchcam7/mhealth/MainActivity.kt
@@ -0,0 +1,19 @@
+package edu.ub.pis2020.gpuchcam7.mhealth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun StartApp(view: View) {
+        //val intent = Intent(this, Menu::class.java)
+        //startActivity(intent)
+    }
+}
