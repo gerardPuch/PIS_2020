897fd2219c98600f0cbc364b960affa727534815
Puki <pukipuch@gmail.com>
2020-04-26 08:57:44 +0200

    Revert "News fragmeent code fill"
    This reverts commit 4b5c54b06323c17343855822a0dfa75ead09186d.

--- MHealth/app/src/main/java/edu/ub/pis2020/gpuchcam7/mhealth/Model/ApiClient.kt
+++ MHealth/app/src/main/java/edu/ub/pis2020/gpuchcam7/mhealth/Model/ApiClient.kt
@@ -1,26 +1,13 @@
 package edu.ub.pis2020.gpuchcam7.mhealth.Model
 
-import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 public class ApiClient {
     private final val BASE_URL: String = "https://newsapi.org/v2/"
-    private var apiClient: ApiClient? = null
-    private var retrofit: Retrofit? = null
+    private val apiClient: ApiClient
+    private val retrofit: Retrofit
 
-    init {
-        retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
-    }
+    private class ApiClient constructor(){
 
-    public fun getInstance(): ApiClient{
-        if (apiClient == null){
-            apiClient = ApiClient()
-        }
-        return apiClient as ApiClient
-    }
-
-    public fun getApi(): ApiInterface{
-        return retrofit!!.create(ApiInterface::class.java)
     }
 }
--- MHealth/app/src/main/java/edu/ub/pis2020/gpuchcam7/mhealth/NewsFragment.kt
+++ MHealth/app/src/main/java/edu/ub/pis2020/gpuchcam7/mhealth/NewsFragment.kt
@@ -5,17 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import edu.ub.pis2020.gpuchcam7.mhealth.Model.Adapter
-import edu.ub.pis2020.gpuchcam7.mhealth.Model.ApiClient
-import edu.ub.pis2020.gpuchcam7.mhealth.Model.Articles
-import edu.ub.pis2020.gpuchcam7.mhealth.Model.Headlines
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,55 +21,13 @@ class NewsFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    /*IMPLEMENTAT*/
-    lateinit var recyclerView: RecyclerView
-    final val API_KEY: String = "52d4d09a2cdc408a8e7b94bac7ba97be" //la meva API KEY
-    lateinit var adapter: Adapter
-    lateinit var articles: List<Articles>
-    /*-----------*/
-
-    /*IMPLEMENTAT*/
-    /*-----------*/
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        /*IMPLEMENTAT*/
-        recyclerView = recyclerView.findViewById(R.id.recyclerView) //???? 34:00
-        recyclerView.layoutManager = LinearLayoutManager(this.context)
-        var country: String = getCountry()
-        retrieveJson(country, API_KEY)
-        /*-----------*/
-
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
     }
-    /*IMPLEMENTAT*/
-    fun retrieveJson(country: String, apiKey: String){
-        var call: Call<Headlines> = ApiClient().getInstance().getApi().getHeadlines(country, apiKey) as Call<Headlines>
-        call.enqueue(object : Callback<Headlines>{
-            override fun onResponse(call: Call<Headlines>, response: Response<Headlines>) {
-                if (response.isSuccessful() && response.body()!!.getArticles() != null){
-                    articles = emptyList()
-                    articles = response.body()!!.getArticles()
-                    adapter = Adapter(context!!, articles)
-                    recyclerView.adapter = adapter
-                }
-            }
-
-            override fun onFailure(call: Call<Headlines>, t: Throwable) {
-                Toast.makeText(context!!, t.localizedMessage, Toast.LENGTH_SHORT).show()
-            }
-        })
-    }
-
-    fun getCountry(): String{
-        var locale: Locale = Locale.getDefault()
-        return "11"
-    }
-    /*-----------*/
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
